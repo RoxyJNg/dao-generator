12e53879b9aa23afb7357b8f4fa9751b06b19dba
DESKTOP-U8F4MUR\Roxy <skyprinceton@hotmail.com>
2019-07-08 22:38:06 +0800

    搭建项目框架

---
+++ dao-generator-jpa/src/main/java/com/null01/DaoGeneratorJpaApplication.java
@@ -0,0 +1,13 @@
+package com.null01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoGeneratorJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaoGeneratorJpaApplication.class, args);
+	}
+
+}
